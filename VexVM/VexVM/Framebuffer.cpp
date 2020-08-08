7a7baf3cd33e06b8c8c17041a9e74634b5c40178
gilmore606 <dan.burford@gmail.com>
2020-08-08 12:29:11 -0700

    bugfix

--- VexVM/VexVM/Framebuffer.cpp
+++ VexVM/VexVM/Framebuffer.cpp
@@ -26,6 +26,7 @@ void Framebuffer::Create() {
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, w, h, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glBindTexture(GL_TEXTURE_2D, 0);
 	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texid, 0);
 	
 	if (glCheckFramebufferStatus(GL_FRAMEBUFFER) == GL_FRAMEBUFFER_COMPLETE) {
