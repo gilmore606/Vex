6443959a06cb6c468c9e243feaf3a5efce213cec
gilmore606 <dan.burford@gmail.com>
2020-08-10 10:50:27 -0700

    add vertbuffer class

---
+++ VexVM/VexVM/Vertbuffer.cpp
@@ -0,0 +1,48 @@
+#include "Vertbuffer.h"
+
+Vertbuffer::Vertbuffer() { }
+
+Vertbuffer::Vertbuffer(GLuint drawtype) { 
+	this->drawtype = drawtype;
+}
+
+void Vertbuffer::addAttrib(int size, GLuint type) {
+	attributes[attribc].size = size;
+	attributes[attribc].type = type;
+	int bytes = 0;
+	if (type == GL_FLOAT) bytes = size * sizeof(float);
+	if (type == GL_INT) bytes = size * sizeof(int);
+	if (type == GL_DOUBLE) bytes = size * sizeof(double);
+	attributes[attribc].bytes = bytes;
+	attribc++;
+}
+
+void Vertbuffer::Create() {
+	glGenVertexArrays(1, &vao);
+	glGenBuffers(1, &vbo);
+	glBindVertexArray(vao);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	int stride = 0;
+	for (int i = 0; i < attribc; i++) {
+		stride += attributes[i].bytes;
+	}
+	int offset = 0;
+	for (int i = 0; i < attribc; i++) {
+		glVertexAttribPointer(i, attributes[i].size, attributes[i].type, GL_FALSE, stride, (void*)offset);
+		glEnableVertexAttribArray(i);
+		offset += attributes[i].bytes;
+	}
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+}
+
+void Vertbuffer::Draw() {
+	glBindVertexArray(vao);
+	glDrawArrays(drawtype, 0, vertc);
+}
+
+void Vertbuffer::loadVertices(float data[], int count, GLuint mode) {
+	vertc = count;
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(data), &data, mode);
+}
---
+++ VexVM/VexVM/Vertbuffer.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <glad/glad.h>
+#include <GLFW\glfw3.h>
+
+struct Vattrib {
+	int size;
+	GLuint type;
+	int bytes;
+};
+
+struct Vertbuffer{
+	Vertbuffer();
+	Vertbuffer(GLuint drawtype);
+
+	void addAttrib(int size, GLuint type);
+	void Create();
+	void loadVertices(float data[], int count, GLuint mode);
+	void Draw();
+
+private:
+	GLuint vao, vbo;
+	Vattrib attributes[8];
+	int attribc = 0;
+	GLuint drawtype;
+	int vertc = 0;
+};
+
