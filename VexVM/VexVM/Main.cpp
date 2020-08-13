3788094d5b9028ae1ad83530610a30790b2dcf1d
gilmore606 <dan.burford@gmail.com>
2020-08-13 00:35:54 -0700

    demo stuff

--- VexVM/VexVM/Main.cpp
+++ VexVM/VexVM/Main.cpp
@@ -11,8 +11,8 @@
 #include "util.cpp"
 
 
-constexpr auto DEMO_POINTS = 500;
-constexpr auto DEMO_LINES = 500;
+constexpr auto DEMO_POINTS = 2000;
+constexpr auto DEMO_LINES = 50;
 
 GLFWwindow* window;
 int windowWidth = 1300;
@@ -31,15 +31,16 @@ struct DemoLine {
 	float dx, dy;
 };
 DemoLine* demoLines = new DemoLine[DEMO_LINES];
-Sprite* demoSprite = new Sprite();
-struct DemoShip {
+Sprite* shipSprite = new Sprite();
+Sprite* rockSprite = new Sprite();
+struct DemoMob {
 	float x = 0.0f;
 	float y = 0.0f;
 	float xd = 0.0f;
 	float yd = 0.0f;
 };
-DemoShip demoShip;
-
+DemoMob demoShip;
+DemoMob demoRock;
 
 
 void onResize(GLFWwindow* window, int w, int h) {
@@ -49,19 +50,37 @@ void onResize(GLFWwindow* window, int w, int h) {
 }
 
 void makeDemoPrims() {
-	demoSprite->add(0.0f, 0.1f, 0.05f, -0.05f, 0.3f, 0.7f, 1.0f);
-	demoSprite->add(0.05f, -0.05f, 0.0f, -0.02f, 0.7f, 0.3f, 0.0f);
-	demoSprite->add(0.0f, -0.02f, -0.05f, -0.05f, 0.7f, 0.3f, 0.0f);
-	demoSprite->add(-0.05f, -0.05f, 0.0f, 0.1f, 0.3f, 0.7f, 1.0f);
-	demoSprite->xs = 0.35f;
-	demoSprite->ys = 0.35f;
+	shipSprite->add(0.0f, 0.1f, 0.05f, -0.05f, 0.3f, 0.7f, 1.0f);
+	shipSprite->add(0.05f, -0.05f, 0.0f, -0.02f, 0.7f, 0.3f, 0.0f);
+	shipSprite->add(0.0f, -0.02f, -0.05f, -0.05f, 0.7f, 0.3f, 0.0f);
+	shipSprite->add(-0.05f, -0.05f, 0.0f, 0.1f, 0.3f, 0.7f, 1.0f);
+	shipSprite->xs = 0.35f;
+	shipSprite->ys = 0.35f;
 
+	rockSprite->add(-0.4f, -0.9f, 0.2f, -0.95f, 0.1f, 0.5f, 0.9f);
+	rockSprite->add(0.2f, -0.95f, 0.7f, -0.8f, 0.1f, 0.5f, 0.9f);
+	rockSprite->add(0.7f, -0.8f, 0.3f, -0.25f, 0.1f, 0.5f, 0.9f);
+	rockSprite->add(0.3f, -0.25f, 0.95f, 0.4f, 0.1f, 0.5f, 0.9f);
+	rockSprite->add(0.95f, 0.4f, 0.45f, 0.9f, 0.1f, 0.5f, 0.9f);
+	rockSprite->add(0.45f, 0.9f, 0.35f, 0.65f, 0.1f, 0.5f, 0.9f);
+	rockSprite->add(0.35f, 0.65f, -0.55f, 0.8f, 0.1f, 0.5f, 0.9f);
+	rockSprite->add(-0.55f, 0.8f, -0.9f, 0.3f, 0.1f, 0.5f, 0.9f);
+	rockSprite->add(-0.9f, 0.3f, -0.8f, -0.5f, 0.1f, 0.5f, 0.9f);
+	rockSprite->add(-0.8f, -0.5f, -0.2f, -0.45f, 0.1f, 0.5f, 0.9f);
+	rockSprite->add(-0.2f, -0.45f, -0.4f, -0.9f, 0.1f, 0.5f, 0.9f);
+	rockSprite->xs = 0.05f;
+	rockSprite->ys = 0.05f;
+	demoRock.xd = 0.1f;
+	demoRock.yd = 0.03f;
+}
+
+void makeDemoClutter() {
 	for (int i = 0; i < DEMO_POINTS; i++) {
 		float x = (randFloat() - 0.5f) * 2.0f;
 		float y = (randFloat() - 0.5f) * 2.0f;
-		float r = randFloat();
-		float g = randFloat();
-		float b = randFloat();
+		float r = randFloat() * 0.4f;
+		float g = randFloat() * 0.7f;
+		float b = randFloat() * 1.9f;
 		float size = randFloat() * 3.5f + 1.0f;
 		demoPoints[i].gpupoint = gpu.addPoint(x,y,r,g,b,size);
 		demoPoints[i].dx = (randFloat() - 0.5f) * 0.5f;
@@ -72,9 +91,9 @@ void makeDemoPrims() {
 		float y1 = (randFloat() - 0.5f) * 2.0f;
 		float x2 = x1 + (randFloat() - 0.5f) * 0.3f;
 		float y2 = y1 + (randFloat() - 0.5f) * 0.3f;
-		float r = randFloat();
+		float r = randFloat() * 1.4f;
 		float g = randFloat();
-		float b = randFloat();
+		float b = randFloat() * 0.5f;
 		demoLines[i].gpuline = gpu.addLine(x1, y1, x2, y2, r, g, b);
 		demoLines[i].dx = (randFloat() - 0.5f) * 0.5f;
 		demoLines[i].dy = (randFloat() - 0.5f) * 0.5f;
@@ -83,16 +102,30 @@ void makeDemoPrims() {
 
 void moveDemoPrims(float delta) {
 
-	if (input.isPressed(1)) demoSprite->rotate(2.5f * delta);
-	if (input.isPressed(2)) demoSprite->rotate(-2.5f * delta);
+	if (input.isPressed(1)) shipSprite->rotate(2.5f * delta);
+	if (input.isPressed(2)) shipSprite->rotate(-2.5f * delta);
 	if (input.isPressed(3)) {
-		demoShip.xd += std::cos(demoSprite->rot + 1.570796) * delta * 1.0f;
-		demoShip.yd += std::sin(demoSprite->rot + 1.570796) * delta * 1.0f;
+		demoShip.xd += std::cos(shipSprite->rot + 1.570796) * delta * 1.0f;
+		demoShip.yd += std::sin(shipSprite->rot + 1.570796) * delta * 1.0f;
 	}
 	demoShip.x += demoShip.xd * delta;
 	demoShip.y += demoShip.yd * delta;
-	demoSprite->moveTo(demoShip.x, demoShip.y);
+	shipSprite->moveTo(demoShip.x, demoShip.y);
+	demoRock.x += demoRock.xd * delta;
+	demoRock.y += demoRock.yd * delta;
+	if (demoRock.x > 1.1) demoRock.x -= 2.2;
+	if (demoRock.x < -1.1) demoRock.x += 2.2;
+	if (demoRock.y > 1.1) demoRock.y -= 2.2;
+	if (demoRock.y < -1.1) demoRock.y += 2.2;
+	if (demoShip.x > 1.1) demoShip.x -= 2.2;
+	if (demoShip.x < -1.1) demoShip.x += 2.2;
+	if (demoShip.y > 1.1) demoShip.y -= 2.2;
+	if (demoShip.y < -1.1) demoShip.y += 2.2;
+	rockSprite->rotate(0.5f * delta);
+	rockSprite->moveTo(demoRock.x, demoRock.y);
+}
 
+void moveDemoClutter(float delta) {
 	for (int i = 0; i < DEMO_POINTS; i++) {
 		demoPoints[i].gpupoint->x += demoPoints[i].dx * delta;
 		demoPoints[i].gpupoint->y += demoPoints[i].dy * delta;
@@ -162,7 +195,9 @@ int main() {
 
 	// Make demo shit
 	makeDemoPrims();
-	gpu.addSprite(demoSprite);
+	makeDemoClutter();
+	gpu.addSprite(shipSprite);
+	gpu.addSprite(rockSprite);
 
 	apu.voices[0].Patch(0.5, 0.5, 0.001, 0.1, 0.5, 0.5, SAWTOOTH, SAWTOOTH, 2.5, 0.0);
 	apu.voices[1].Patch(0.5, 0.4, 0.04, 1.2, 0.3, 0.1, PULSE, PULSE, 1.1, 0.01);
@@ -184,6 +219,7 @@ int main() {
 		lastFrame = currentFrame;
 
 		moveDemoPrims(deltaTime);
+		moveDemoClutter(deltaTime);
 
 		gpu.Assemble();
 		gpu.Render();
