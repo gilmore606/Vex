1c198c2c7a63df78cec9c5b685d3f8e5187047db
gilmore606 <dan.burford@gmail.com>
2020-09-05 01:14:55 -0700

    math funcs

--- Vexer/src/compiler/Syscalls.kt
+++ Vexer/src/compiler/Syscalls.kt
@@ -1,43 +1,78 @@
 package compiler
 
+import compiler.Coder
+import compiler.CompileException
+import compiler.FuncSig
+import compiler.Meaner
+import compiler.Opcode.*
+import compiler.*
+import compiler.ValueType.*
+
 object Syscalls {
 
     val funcs = FUNCLIST().apply {
-        add(FuncSig(true, "V", 0, ValueType.VAL_VECTOR, null,
-                TYPELIST().apply { add(ValueType.VAL_FLOAT); add(ValueType.VAL_FLOAT) }))
-        add(FuncSig(true, "C", 1, ValueType.VAL_COLOR, null,
-                TYPELIST().apply { add(ValueType.VAL_FLOAT); add(ValueType.VAL_FLOAT); add(ValueType.VAL_FLOAT) }))
-        add(FuncSig(true, "RAND", 2, ValueType.VAL_FLOAT, null,
-                TYPELIST().apply { add(ValueType.VAL_FLOAT); add(ValueType.VAL_FLOAT) }))
-        add(FuncSig(true, "RAND", 3, ValueType.VAL_FLOAT, null,
-                TYPELIST().apply { add(ValueType.VAL_FLOAT) }))
-        add(FuncSig(true, "RAND", 4, ValueType.VAL_INT, null,
-                TYPELIST().apply { add(ValueType.VAL_INT); add(ValueType.VAL_INT) }))
-        add(FuncSig(true, "RAND", 5, ValueType.VAL_INT, null,
-                TYPELIST().apply { add(ValueType.VAL_INT) }))
-        add(FuncSig(true, "RAND", 6, ValueType.VAL_FLOAT, null,
+        add(FuncSig(true, "V", 0, VAL_VECTOR, null,
+                TYPELIST().apply { add(VAL_FLOAT); add(VAL_FLOAT) }))
+        add(FuncSig(true, "C", 1, VAL_COLOR, null,
+                TYPELIST().apply { add(VAL_FLOAT); add(VAL_FLOAT); add(VAL_FLOAT) }))
+        add(FuncSig(true, "RAND", 2, VAL_FLOAT, null,
+                TYPELIST().apply { add(VAL_FLOAT); add(VAL_FLOAT) }))
+        add(FuncSig(true, "RAND", 3, VAL_FLOAT, null,
+                TYPELIST().apply { add(VAL_FLOAT) }))
+        add(FuncSig(true, "RAND", 4, VAL_INT, null,
+                TYPELIST().apply { add(VAL_INT); add(VAL_INT) }))
+        add(FuncSig(true, "RAND", 5, VAL_INT, null,
+                TYPELIST().apply { add(VAL_INT) }))
+        add(FuncSig(true, "RAND", 6, VAL_FLOAT, null,
                 TYPELIST() ))
-        add(FuncSig(true, "TEXT", 7, ValueType.VAL_OBJECT, ObjectType.OBJ_SPRITE,
-                TYPELIST().apply { add(ValueType.VAL_STRING) }))
+        add(FuncSig(true, "MIN", 7, VAL_INT, null, 
+                TYPELIST().apply { add(VAL_INT); add(VAL_INT) } ))
+        add(FuncSig(true, "MAX", 8, VAL_INT, null,
+                TYPELIST().apply { add(VAL_INT); add(VAL_INT) } ))
+        add(FuncSig(true, "ABS", 9, VAL_INT, null,
+                TYPELIST().apply { add(VAL_INT) } ))
+        add(FuncSig(true, "SIGN", 10, VAL_INT, null,
+                TYPELIST().apply { add(VAL_INT) } ))
+        add(FuncSig(true, "MIN", 11, VAL_FLOAT, null,
+                TYPELIST().apply { add(VAL_FLOAT); add(VAL_FLOAT) } ))
+        add(FuncSig(true, "MAX", 12, VAL_FLOAT, null,
+                TYPELIST().apply { add(VAL_FLOAT); add(VAL_FLOAT) } ))
+        add(FuncSig(true, "ABS", 13, VAL_FLOAT, null,
+                TYPELIST().apply { add(VAL_FLOAT) } ))
+        add(FuncSig(true, "SIGN", 14, VAL_INT, null,
+                TYPELIST().apply { add(VAL_FLOAT) } ))
+        add(FuncSig(true, "SIN", 15, VAL_FLOAT, null,
+                TYPELIST().apply { add(VAL_FLOAT) } ))
+        add(FuncSig(true, "COS", 16, VAL_FLOAT, null,
+                TYPELIST().apply { add(VAL_FLOAT) } ))
+        add(FuncSig(true, "CLAMP", 17, VAL_INT, null,
+                TYPELIST().apply { add(VAL_INT); add(VAL_INT); add(VAL_INT) } ))
+        add(FuncSig(true, "CLAMP", 18, VAL_FLOAT, null,
+                TYPELIST().apply { add(VAL_FLOAT); add(VAL_FLOAT); add(VAL_FLOAT) }))
+        add(FuncSig(true, "SQRT", 19, VAL_FLOAT, null,
+                TYPELIST().apply { add(VAL_FLOAT) }))
+
+        add(FuncSig(true, "TEXT", 30, VAL_OBJECT, ObjectType.OBJ_SPRITE,
+                TYPELIST().apply { add(VAL_STRING) }))
     }
 
     val classes = HashMap<ObjectType, FUNCLIST>().also {
 
         it[ObjectType.OBJ_SPRITE] = FUNCLIST().apply {
-            add(FuncSig(true, "move", 0, ValueType.VAL_NIL, null,
-                    TYPELIST().apply { add(ValueType.VAL_FLOAT); add(ValueType.VAL_FLOAT) }))
-            add(FuncSig(true, "move", 1, ValueType.VAL_NIL, null,
-                    TYPELIST().apply { add(ValueType.VAL_VECTOR) }))
-            add(FuncSig(true, "scale", 2, ValueType.VAL_NIL, null,
-                    TYPELIST().apply { add(ValueType.VAL_FLOAT); add(ValueType.VAL_FLOAT) }))
-            add(FuncSig(true, "scale", 3, ValueType.VAL_NIL, null,
-                    TYPELIST().apply { add(ValueType.VAL_FLOAT) }))
-            add(FuncSig(true, "hide", 4, ValueType.VAL_NIL, null,
+            add(FuncSig(true, "move", 0, VAL_NIL, null,
+                    TYPELIST().apply { add(VAL_FLOAT); add(VAL_FLOAT) }))
+            add(FuncSig(true, "move", 1, VAL_NIL, null,
+                    TYPELIST().apply { add(VAL_VECTOR) }))
+            add(FuncSig(true, "scale", 2, VAL_NIL, null,
+                    TYPELIST().apply { add(VAL_FLOAT); add(VAL_FLOAT) }))
+            add(FuncSig(true, "scale", 3, VAL_NIL, null,
+                    TYPELIST().apply { add(VAL_FLOAT) }))
+            add(FuncSig(true, "hide", 4, VAL_NIL, null,
                     TYPELIST()))
-            add(FuncSig(true, "show", 5, ValueType.VAL_NIL, null,
+            add(FuncSig(true, "show", 5, VAL_NIL, null,
                     TYPELIST()))
-            add(FuncSig(true, "write", 6, ValueType.VAL_NIL, null,
-                    TYPELIST().apply { add(ValueType.VAL_STRING) }))
+            add(FuncSig(true, "write", 6, VAL_NIL, null,
+                    TYPELIST().apply { add(VAL_STRING) }))
         }
 
     }
